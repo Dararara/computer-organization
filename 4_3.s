e11b90cd05d2844706311fb50d798b35826a1b46
Dararara <1402982842@qq.com>
2019-03-22 10:56:49 +0800

    should work now

--- 4_3.s
+++ 4_3.s
@@ -2,30 +2,53 @@
 .data
 .text
 	jal main
+	end
+	
+	
 	
 main:
-	
+	addi $sp, $sp, -4
+	sw $ra, ($sp)
 	li $t0, 0#count 100 number
-	li $t1, 1#iterate integer
+	li $t1, 2#iterate integer
 	loop:
 	beq $t0, 100, endl
 	add $a1, $t1, $zero
 	jal test_prime
 	beq $v1, 0, next
 	#print the prime
-	add $a0, $v1, $zero
+	add $a0, $t1, $zero
 	li $v0, 1
 	syscall
+	print_string("\n")
 	addi $t0, $t0, 1
 	next:
 	addi $t1, $t1, 1
 	j loop
-	endl
+	endl:
 	
+	lw $ra, ($sp)
 	jr $ra
 	
 	
 #use a1 as n return in v1
 test_prime:
 
-	li $v1, 1
+	li $t2, 2
+	loop_test:
+	slt $t3, $t2, $a1
+	beqz $t3, return1
+	divu $a1, $t2
+	mfhi $t3
+	beqz $t3, return0
+	addi $t2, $t2, 1
+	j loop_test
+	
+	
+	return1:
+	li $v1,1
+	jr $ra
+	
+	return0:
+	li $v1, 0
+	jr $ra
